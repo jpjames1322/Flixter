35fa989f38885d2e8e20f53736f2e1f13d4eb51e
jared-phillips <jp.james0927@gmail.com>
2019-10-04 01:09:02 +0000

    permission

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,9 +1,13 @@
 class Instructor::CoursesController < ApplicationController
+   class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def new
     @course = Course.new
   end
-   def create
+
+  def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
       redirect_to instructor_course_path(@course)
@@ -11,11 +15,22 @@ class Instructor::CoursesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
