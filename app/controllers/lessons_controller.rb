cfd948f84e39c3e0a64a17a91fa6bdf512306440
jared-phillips <jp.james0927@gmail.com>
2019-10-13 23:43:55 +0000

    payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson
   def show
   end
   
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.user != current_user
+      redirect_to course_sections, alert: 'Error Message Here'
+    end
+  end
+
+  def require_enrollment_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to view this lesson."
+    end
+  end
+
 
   helper_method :current_lesson
   def current_lesson
