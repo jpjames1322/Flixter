b3f011914771190ff19676c3b72b6740ff09f196
jared-phillips <jp.james0927@gmail.com>
2019-10-29 21:20:26 +0000

    bug?

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+  resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :lessons, only: [:update]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create] 
     end
   end
 end
